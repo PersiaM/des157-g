b62654506104e0a4a96677abfd920f453467a0ad
PersiaM <persiamasoudi@gmail.com>
2019-02-06 09:09:25 -0800

    init

---
+++ studio2/script.js
@@ -0,0 +1,6 @@
+var rectangle = document.querySelector('#rectangle');
+var overlay = document.querySelector('#overlay');
+
+rectangle.addEventListener ('click', function () {
+  overlay.style.display='block';
+},false);
